3b961e1472b0e33349c8d722f4fbae61eff1c1f1
Chris Whong <chris.m.whong@gmail.com>
2019-07-24 16:37:35 -0400

    feat: add headers to body table

--- app/containers/BodyContainer.tsx
+++ app/containers/BodyContainer.tsx
@@ -1,13 +1,33 @@
 import { connect } from 'react-redux'
 import Body from '../components/Body'
-import Store from '../models/store'
+import Store, { WorkingDataset } from '../models/store'
 import { fetchBody } from '../actions/api'
+
+const extractColumnHeaders = (workingDataset: WorkingDataset): undefined | object => {
+  const { structure } = workingDataset.value
+  if (!structure) {
+    return undefined
+  }
+
+  const { schema } = structure
+
+  if (schema && (!schema.items || (schema.items && !schema.items.items))) {
+    return undefined
+  }
+
+  return schema && schema.items && schema.items.items.map((d: { title: string }): string => d.title)
+}
 
 const mapStateToProps = (state: Store) => {
   const { workingDataset } = state
 
+  const headers = extractColumnHeaders(workingDataset)
+
   // get data for the currently selected component
-  return { workingDataset }
+  return {
+    workingDataset,
+    headers
+  }
 }
 
 const actions = {
