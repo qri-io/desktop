5cba5d3b3fb601af94125f614dcf40e8d7095579
Chris Whong <chris.m.whong@gmail.com>
2019-08-01 18:02:59 -0400

    wip: detect scroll to bottom of handsontable

--- app/components/HandsonTable.tsx
+++ app/components/HandsonTable.tsx
@@ -7,6 +7,28 @@ interface HandsonTableProps {
 }
 
 export default class HandsonTable extends React.Component<HandsonTableProps> {
+  private hotRef: React.RefObject<HotTable>;
+  constructor (props: HandsonTableProps) {
+    super(props)
+    this.hotRef = React.createRef()
+  }
+
+  handleVerticalScroll () {
+    if (this.hotRef.current) {
+      const { hotInstance } = this.hotRef.current
+      const autoRowSize = hotInstance.getPlugin('autoRowSize')
+
+      const totalRows = hotInstance.countRows()
+      const lastVisibleRow = autoRowSize.getLastVisibleRow() + 1 // add 1 because it's zero indexed
+
+      if (lastVisibleRow === totalRows) console.log('YOU ARE AT THE BOTTOM OF THE LIST, GET DATA NOW!!!!!')
+    }
+  }
+
+  componentDidMount () {
+
+  }
+
   render () {
     const { body } = this.props
     const headers = this.props.headers ? this.props.headers : true
@@ -14,6 +36,7 @@ export default class HandsonTable extends React.Component<HandsonTableProps> {
     return (
       <div>
         <HotTable
+          ref={this.hotRef}
           data={body}
           colHeaders={headers}
           rowHeaders
@@ -24,6 +47,8 @@ export default class HandsonTable extends React.Component<HandsonTableProps> {
           viewportColumnRenderingOffset={100}
           viewportRowRenderingOffset={100}
           columnSorting
+          autoRowSize
+          afterScrollVertically={() => { this.handleVerticalScroll() }}
         />
       </div>
     )
