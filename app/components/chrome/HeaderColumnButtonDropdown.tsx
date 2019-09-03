617ad59873a0d3cb63dccefab09846e62a4261ae
Chris Whong <chris.m.whong@gmail.com>
2019-09-03 16:26:39 -0400

    fix: header dropdown css

--- app/components/chrome/HeaderColumnButtonDropdown.tsx
+++ app/components/chrome/HeaderColumnButtonDropdown.tsx
@@ -12,30 +12,24 @@ const HeaderColumnButtonDropdown: React.FunctionComponent<HeaderColumnButtonDrop
     setShowMenu(!showMenu)
     event.stopPropagation()
   }
-  const { icon = '', label = '', tooltip, onClick = toggleMenu, items } = props
-
-  const twoActionStyle = {
-    backgroundColor: 'rgba(0,0,0,0.1)',
-    width: 38,
-    height: '100%'
-  }
+  const { icon, label = '', tooltip, onClick = toggleMenu, items } = props
 
   return (
     <div
-      className='header-column'
+      className='header-column header-column-dropdown'
       data-tip={tooltip}
       onClick={onClick}
     >
-      {(icon !== '') && (typeof icon === 'string')
-        ? <div className='header-column-icon'><FontAwesomeIcon icon={icon} size='lg'/></div>
-        : icon
+      {React.isValidElement(icon)
+        ? icon
+        : <div className='header-column-icon'><FontAwesomeIcon icon={icon} size='lg'/></div>
       }
       {(label !== '') &&
         <div className='header-column-text'>
           <div className='label'>{label}</div>
         </div>
       }
-      <div className='header-column-arrow' onClick={toggleMenu} style={(onClick === toggleMenu) ? undefined : twoActionStyle }>
+      <div className='header-column-arrow' onClick={toggleMenu}>
         {showMenu
           ? <div className="arrow collapse">&nbsp;</div>
           : <div className="arrow expand">&nbsp;</div>
