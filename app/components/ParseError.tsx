0ab00e03ddf2ec61e8fe875360b4f555d32c339d
ramfox <kasey@qri.io>
2019-09-05 17:07:17 -0400

    fix(ComponentList): `Open in Finder` command finder opens properly

--- app/components/ParseError.tsx
+++ app/components/ParseError.tsx
@@ -15,7 +15,7 @@ const ParseError: React.FunctionComponent<ParseErrorProps> = ({ component, filen
     <div className={'message-container'}>
       <div>
         <h4>There are parsing errors in your {component}</h4>
-        <p>Fix the errors in your <strong>{filename}</strong> file to be able to view the file in Qri Desktop or to save a version of your dataset.</p>
+        <p>Fix the errors in your <strong>{filename}</strong> file to be able to view it in Qri Desktop.</p>
         <Button text="Show in Finder" color="primary" onClick={() => { shell.openItem(linkpath) }} />
       </div>
     </div>
