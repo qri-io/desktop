c023b3aaa9adae304e0c8d61bd456398ffa85e76
ramfox <kasey@qri.io>
2019-09-10 16:56:59 -0400

    refactor(selections): all actions that should effect selection are caught in selection reducer
    refer to #212

---
+++ test/app/utils/chooseDefaultComponent.test.ts
@@ -0,0 +1,43 @@
+import chooseDefaultComponent from '../../../app/utils/chooseDefaultComponent'
+import { Dataset } from '../../../app/models/dataset'
+
+describe('chooseDefaultComponent', () => {
+  const cases = [
+    {
+      describe: 'full dataset (has meta, body, and schema)',
+      dataset: {meta: {title: 'test'}, schema: {$id:'test'}, body: {foo: 'bar'}, structure: {foo: 'bar'}, commit: {foo: 'bar'}},
+      component: 'meta', 
+    },
+    {
+      describe: 'dataset with body and schema',
+      dataset: {schema: {$id:'test'}, body: {foo: 'bar'}},
+      component: 'body', 
+    },
+    {
+      describe: 'dataset with schema',
+      dataset: {schema: {$id:'test'}},
+      component: 'schema', 
+    },
+    {
+      describe: 'dataset with structure',
+      dataset: {structure: {$id:'test'}},
+      component: '', 
+    },
+    {
+      describe: 'dataset is empty object',
+      dataset: {},
+      component: '', 
+    },
+    {
+      describe: 'dataset is undefined',
+      dataset: undefined,
+      component: '', 
+    }
+  ]
+  cases.forEach(({describe, dataset, component}) => {
+    it (`case '${describe}'`, () => {
+      const got = chooseDefaultComponent(dataset as Dataset)
+      expect(got).toBe(component)
+    })
+  })
+})
