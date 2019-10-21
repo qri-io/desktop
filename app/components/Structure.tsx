f307e1511c2624e1a413d5e4596bfe1a6eb0a74f
ramfox <kasey@qri.io>
2019-10-21 14:30:20 -0400

    refactor(Structure): use `FormatConfigFSI` and `FormatConfigHistory`
    Switch on `history` to display either `FormatConfigFSI` or `FormatConfigHistory`.
    
    When we are in `status` aka `FSI` world, let's always show format config options based on the _extension of the body file_, that way we can respond immediately if the user has changed the bodyfile.

--- app/components/Structure.tsx
+++ app/components/Structure.tsx
@@ -4,74 +4,26 @@ import { Structure as IStructure } from '../models/dataset'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import ExternalLink from './ExternalLink'
-import TextInput from './form/TextInput'
 import { ApiActionThunk } from '../store/api'
+import FormatConfigHistory from './FormatConfigHistory'
+import FormatConfigFSI from './FormatConfigFSI'
 
 export interface StructureProps {
   peername: string
   name: string
   structure: IStructure
   history: boolean
+  fsiBodyFormat: string
   write: (peername: string, name: string, dataset: any) => ApiActionThunk
 }
 
-interface InputCheckboxProps {
-  key: number
-  history: boolean
-  value: boolean
-  label: string
-  tooltip: string
-  name: string
-  onChange: (name: string, value: any) => void
-}
-
-const InputCheckbox: React.FunctionComponent<InputCheckboxProps> = ({ key, history, value, label, tooltip, onChange, name }) => {
-  const [checked, setChecked] = React.useState(value)
-
-  React.useEffect(() => {
-    if (checked !== value) setChecked(value)
-  }
-  , [value])
-  return (
-    <div className='input-checkbox' key={key}>
-      <div><input type="checkbox" id="headerRow" name={name} onChange={() => {
-        const val = checked
-        setChecked(!val)
-        onChange(name, !val)
-      }} disabled={history} checked={checked} />&nbsp;&nbsp;</div>
-      <div>{label}&nbsp;</div>
-      <span
-        data-tip={tooltip}
-        className='text-input-tooltip'
-      >
-        <FontAwesomeIcon icon={faInfoCircle} size='sm'/>
-      </span>
-    </div>
-  )
-}
-
-interface FormatConfigOption {
+export interface FormatConfigOption {
   label: string
   tooltip: string
   type: string
 }
 
-const csvFormatConfig: {[key: string]: string | boolean} = {
-  headerRow: false,
-  variadicFields: false,
-  lazyQuotes: false
-  // separator: ","
-}
-
-const jsonFormatConfig: {[key: string]: string | boolean} = {
-  pretty: false
-}
-
-const xlsxFormatConfig: {[key: string]: string | boolean} = {
-  sheetName: ''
-}
-
-const formatConfigOptions: { [key: string]: FormatConfigOption } = {
+export const formatConfigOptions: { [key: string]: FormatConfigOption } = {
   headerRow: {
     label: 'This table contains a header row',
     tooltip: 'If the the first row of the table IS the row containing all the column names, check this input',
@@ -107,46 +59,17 @@ const formatConfigOptions: { [key: string]: FormatConfigOption } = {
   }
 }
 
-const Structure: React.FunctionComponent<StructureProps> = ({ peername, name, structure, history, write }) => {
-  // if we are displaying a structure from a moment in history
-  // only show the config options that were set to true
-  const renderInputCheckbox = (key: number, history: boolean, value: boolean, label: string, tooltip: string, onChange: (name: string, value: any) => void, name: string) => {
-    if (history) {
-      if (value) return <div key={key} className='config-item margin-bottom'>{label}</div>
-      return undefined
-    }
-    return (
-      <InputCheckbox
-        name={name}
-        key={key}
-        history={history}
-        value={value}
-        label={label}
-        tooltip={tooltip}
-        onChange={onChange}
-      />)
-  }
-
-  const renderTextInput = (key: number, history: boolean, value: string, label: string, tooltip: string, onChange: (name: string, value: any) => void, name: string) => {
-    if (history) return <div className='config-item margin-bottom'>{label}: {value}</div>
-    return (
-      <TextInput
-        key={key}
-        label={label}
-        labelTooltip={tooltip}
-        onChange={onChange}
-        value={value}
-        name={name}
-        type='text'
-        maxLength={600}
-      />
-    )
-  }
-
+const Structure: React.FunctionComponent<StructureProps> = ({ peername, name, structure, history, write, fsiBodyFormat }) => {
+  const format = history ? structure.format : fsiBodyFormat
   const handleWrite = (option: string, value: any) => {
+    // TODO (ramfox): sending over format since a user can replace the body with a body of a different
+    // format. Let's pass in whatever the current format is, so that we have unity between
+    // what the desktop is seeing and the backend. This can be removed when we have the fsi
+    // backend codepaths settled
     write(peername, name, {
       structure: {
         ...structure,
+        format,
         formatConfig: {
           ...structure.formatConfig,
           [option]: value
@@ -154,72 +77,16 @@ const Structure: React.FunctionComponent<StructureProps> = ({ peername, name, st
       }
     })
   }
-
-  var formatConfig: { [key: string]: string | boolean }
-  const format = structure && structure.format
-  if (history) formatConfig = structure.formatConfig
-  else {
-    switch (format) {
-      case 'csv':
-        formatConfig = {
-          ...csvFormatConfig,
-          ...structure.formatConfig
-        }
-        break
-      case 'json':
-        formatConfig = {
-          ...jsonFormatConfig,
-          ...structure.formatConfig
-        }
-        break
-      case 'xlsx':
-        formatConfig = {
-          ...xlsxFormatConfig,
-          ...structure.formatConfig
-        }
-        break
-      default:
-        formatConfig = {}
-    }
-  }
   return (
     <div className='structure content'>
-      <div>
-        <h4 className='config-title'>
-          {format ? format.toUpperCase() + ' ' : ''}Configuration
-        </h4>
-        <div>
-          { Object.keys(formatConfig).length === 0
-            ? <div className='margin'>No configurations details</div>
-            : Object.keys(formatConfigOptions).map((option: string, i) => {
-              if (option in formatConfig) {
-                switch (formatConfigOptions[option].type) {
-                  case 'boolean':
-                    return renderInputCheckbox(
-                      i,
-                      history,
-                      !!formatConfig[option],
-                      formatConfigOptions[option].label,
-                      formatConfigOptions[option].tooltip,
-                      handleWrite,
-                      option
-                    )
-                  case 'string':
-                    return renderTextInput(
-                      i,
-                      history,
-                      formatConfig[option].toString(),
-                      formatConfigOptions[option].label,
-                      formatConfigOptions[option].tooltip,
-                      handleWrite,
-                      option
-                    )
-                }
-              }
-              return undefined
-            })}
-        </div>
-      </div>
+      { history
+        ? <FormatConfigHistory structure={structure} />
+        : <FormatConfigFSI
+          structure={structure}
+          format={format}
+          write={handleWrite}
+        />
+      }
       <div>
         <h4 className='schema-title'>
           Schema
--- app/containers/StructureContainer.tsx
+++ app/containers/StructureContainer.tsx
@@ -2,14 +2,24 @@ import { connect } from 'react-redux'
 import Structure, { StructureProps } from '../components/Structure'
 import Store from '../models/store'
 import { fsiWriteAndFetch } from '../actions/api'
+import path from 'path'
 
-const mapStateToProps = (state: Store, ownProps: StructureProps) => {
+const mapStateToProps = (state: Store) => {
   const { workingDataset, commitDetails, selections } = state
 
+  // get if history
+  const history = selections.activeTab === 'history'
+  // if history, get fsiBodyFormat
+  var fsiBodyFormat = ''
+  if (!history) {
+    fsiBodyFormat = state.workingDataset.status && state.workingDataset.status.body && state.workingDataset.status.body.filepath
+      ? path.extname(state.workingDataset.status.body.filepath).slice(1)
+      : ''
+  }
   // get data for the currently selected component
-  const structure = ownProps.history ? commitDetails.components.structure.value : workingDataset.components.structure.value
+  const structure = history ? commitDetails.components.structure.value : workingDataset.components.structure.value
   const schema = 'schema' in structure ? structure.schema : {}
-  return { schema, structure, peername: selections.peername, name: selections.name, history: selections.activeTab === 'history' }
+  return { schema, structure, peername: selections.peername, name: selections.name, history, fsiBodyFormat }
 }
 
 const actions = {
