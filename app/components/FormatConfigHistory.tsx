d633a9e556afaa3bd5ef552faf8c05eb23f4f0c4
ramfox <kasey@qri.io>
2019-10-21 14:28:45 -0400

    feat(FormatConfig): add `FormatConfigFSI` and `FormatConfigHistory`
    Because there is no overlap between how we will be displaying the FormatConfig when we are in the status tab and the history tab, to simplify the logic in our components, we are adding `FormatConfigFSI` and `FormatConfigHistory` components. Each imports the `FormatConfigOptions` object from `Structure`. The `FSI` version shows inputs and allows an `onChange` funciton. The `History` version just displays the FormatConfig text.
    
    Another major difference between these two components is the `FSI` version will always show all the options that can exist for this format in the `FormatConfig`, history will only show the options that are set.

---
+++ app/components/FormatConfigHistory.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import { Structure as IStructure } from '../models/dataset'
+import { formatConfigOptions } from './structure'
+
+export interface FormatConfigHistoryProps {
+  structure: IStructure
+}
+
+const FormatConfigHistory: React.FunctionComponent<FormatConfigHistoryProps> = ({ structure }) => {
+  // if we are displaying a structure from a moment in history
+  // only show the config options that were set to true
+  const format = structure.format
+  const formatConfig = structure.formatConfig
+  return (
+    <div>
+      <h4 className='config-title'>
+        {format ? format.toUpperCase() + ' ' : ''}Configuration
+      </h4>
+      <div>
+        { (formatConfig === undefined || formatConfig === null || Object.keys(formatConfig).length === 0)
+          ? <div className='margin'>No configurations details</div>
+          : Object.keys(formatConfigOptions).map((option: string, i) => {
+            if (option in formatConfig) {
+              switch (formatConfigOptions[option].type) {
+                case 'boolean':
+                  return formatConfig[option] && <div key={`structure_${i}`} className='config-item margin-bottom'>{formatConfigOptions[option].label}</div>
+                case 'string':
+                  return <div key={`structure_${i}`} className='config-item margin-bottom'>{formatConfigOptions[option].label}: {formatConfig[option]}</div>
+              }
+            }
+            return undefined
+          })}
+      </div>
+    </div>
+  )
+}
+
+export default FormatConfigHistory
