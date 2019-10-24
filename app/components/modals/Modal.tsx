0bd27c1c03200d3471ceb0314c704e3d481c0981
Dave Foster <dave@axiomdatascience.com>
2019-10-24 14:53:28 -0400

    Fix type-sensitive imports

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import classNames from 'classnames'
-import ModalHeader from './header'
+import ModalHeader from './Header'
 import ReactTooltip from 'react-tooltip'
 
 /**
