7c913869ffb2c272a407d56030d5e5e191468306
ramfox <kasey@qri.io>
2019-11-04 16:35:08 -0500

    fix(bodyValue): adjust `bodyValue` func for case where we reset pagination
    When we are purposely reseting pagination, we have `pageInfo.page === 1`. More subtly, we need to account for when the `previous` body is actually the call `pageInfo.page === 2`, in that case, we shouldn't reset the entire body, but continue through the code path to return the `current` body before the previous body.
    
    Also, let's remove all functions (`concat`, `splice`) that mutate the passed in body, as this may mutate state,

--- app/utils/bodyValue.ts
+++ app/utils/bodyValue.ts
@@ -17,6 +17,7 @@ export default function bodyValue (prev: Object | any[] | undefined, curr: Objec
       // shim to insert a unique index as the first item in each Array (row)
       // useful for a react element key to keep track of rendered rows while we paginate
       const { page, pageSize } = pageInfo
+
       curr.forEach((d, i) => {
         const index = ((page - 1) * pageSize) + i
         d.unshift(index)
@@ -25,24 +26,33 @@ export default function bodyValue (prev: Object | any[] | undefined, curr: Objec
       if (prev.length !== 0) {
         const currLastIndex = curr[curr.length - 1][0]
         const prevLastIndex = prev[prev.length - 1][0] || 0
+        const prevFirstIndex = prev[0][0]
+
+        // if the prevFirstIndex equals page size, and page is equal to 1, then
+        // we have scrolled up from fetching the second page. We should therefore
+        // keep the previous page
+        // however, if page is 1 and the prevFirstIndex is not equal to pageSize,
+        // (meaning the previous call was not to the second page, so we most
+        // likely reset the pagination) we should return just the current body
+        if (page === 1 && prevFirstIndex !== pageSize) {
+          return curr
+        }
 
         // user scrolled up and got an earlier page
         if (currLastIndex < prevLastIndex) {
-          prev.splice(pageSize, pageSize)
-
-          return curr.concat(prev)
+          return [...curr, ...prev.slice(0, pageSize)]
         }
       }
 
       // check length of prev, remove 1 pageSize of rows from the beginning if greater than 2 pageSizes
       if (prev.length === (pageSize * 2)) {
-        prev.splice(0, pageSize)
+        return [...prev.slice(pageSize, pageSize * 2), ...curr]
       }
 
-      return prev.concat(curr)
+      return [...prev, ...curr]
     }
 
-    return prev.concat(curr)
+    return [...prev, ...curr]
   }
   // work through cases where the current body value is an object (but not an array)
   if (typeof curr === 'object') {
