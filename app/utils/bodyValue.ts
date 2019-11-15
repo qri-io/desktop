afef2ed40817227ec6abb544ab6581510fb39afb
ramfox <kasey@qri.io>
2019-11-15 14:18:44 -0500

    fix(bodyValue): account for null or undefined previous body values

--- app/utils/bodyValue.ts
+++ app/utils/bodyValue.ts
@@ -1,16 +1,12 @@
 import { PageInfo } from '../../app/models/store'
 
 export default function bodyValue (prev: Object | any[] | undefined, curr: Object | any[], pageInfo: PageInfo): Object | any[] {
-  // if there is no previous body value, just return the current body value
-  if (!prev) return curr
   // work through cases where the current body value is an array
   if (Array.isArray(curr)) {
-    // if the previous body value is not an array
-    // just return the current body value
-    if (!Array.isArray(prev)) {
-      return curr
+    if (curr.length === 0) {
+      if (!prev) return curr
+      return prev
     }
-    // otherwise, it is an array and we should concat the lists
 
     if (Array.isArray(curr[0])) {
       // for tabular/2D arrays only:
@@ -22,6 +18,9 @@ export default function bodyValue (prev: Object | any[] | undefined, curr: Objec
         const index = ((page - 1) * pageSize) + i
         d.unshift(index)
       })
+
+      // if there is no previous body value, just return the current body value
+      if (!prev || !Array.isArray(prev)) return curr
 
       if (prev.length !== 0) {
         const currLastIndex = curr[curr.length - 1][0]
@@ -51,14 +50,12 @@ export default function bodyValue (prev: Object | any[] | undefined, curr: Objec
 
       return [...prev, ...curr]
     }
-
-    return [...prev, ...curr]
   }
   // work through cases where the current body value is an object (but not an array)
   if (typeof curr === 'object') {
     // if the previous body value is not an object (or is an object but is also an array)
     // return the current body value
-    if (!(typeof prev === 'object') && Array.isArray(prev)) {
+    if (!prev || (!(typeof prev === 'object') && Array.isArray(prev))) {
       return curr
     }
     // otherwise return the spread!
