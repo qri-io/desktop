8111c22ba592a9220a445b489e16ef49b5166455
Chris Whong <chris.m.whong@gmail.com>
2019-12-02 15:56:42 -0500

    feat: add dataset rename

--- app/components/DatasetReference.tsx
+++ app/components/DatasetReference.tsx
@@ -82,7 +82,11 @@ const DatasetReference: React.FunctionComponent<DatasetReferenceProps> = (props)
     setNewName(value)
   }
 
+<<<<<<< HEAD
   // when the input is focused, scroll all the way to the left
+=======
+  // when the input is focused, set the cursor to the left, and scroll all the way to the left
+>>>>>>> feat: add dataset rename
   const onFocus = () => {
     const el = document.getElementById('dataset-name-input') as HTMLInputElement
     el.scrollLeft = 0
