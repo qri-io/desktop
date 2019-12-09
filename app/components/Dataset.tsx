700a9da5c458a9b3254064a01c93df081f8cfca3
ramfox <kasey@qri.io>
2019-12-09 17:10:45 -0500

    refactor(status, UnlinkedDataset): change wording to 'checkout'
    Added more explanation to the `UnlinkedDataset` section, not married to any verbage to happy to change as needed.
    
    When the dataset is not checked out, we have all the components listed, but disabled.

--- app/components/Dataset.tsx
+++ app/components/Dataset.tsx
@@ -186,8 +186,8 @@ class Dataset extends React.Component<DatasetProps> {
         />) : (
         <HeaderColumnButton
           id='linkButton'
-          label='link to filesystem'
-          tooltip='Link this dataset to a folder on your computer'
+          label='checkout'
+          tooltip='Checkout this dataset to a folder on your computer'
           icon={(
             <span className='fa-layers fa-fw'>
               <FontAwesomeIcon icon={faFile} size='lg'/>
