78060f806e350fcc81e401d67de77914fcbe4f6a
ramfox <kasey@qri.io>
2020-01-06 12:05:40 -0500

    test(e2e): refactoring e2e utility functions to separate file The e2e test utility functions are now in their own file. To use, you must import the `newE2ETestUtils` function, pass in the initialized `app` object, and destructure the return value (an object that satisfies the `E2ETestUtil` interface.  Please enter the commit message for your changes. Lines starting

--- test/e2e/e2e.spec.ts
+++ test/e2e/e2e.spec.ts
@@ -1,16 +1,20 @@
 import path from 'path'
-import url from 'url'
 import fs from 'fs'
 import TestBackendProcess from '../utils/testQriBackend'
 import fakeDialog from 'spectron-fake-dialog'
+import { E2ETestUtils, newE2ETestUtils } from '../utils/e2eTestUtils'
 
 const { Application } = require('spectron')
-
-const delay = async (time: number) => new Promise(resolve => setTimeout(resolve, time))
 
 describe('Qri End to End tests', function spec () {
   let app: any
   let backend: any
+
+  // The utility functions we use to build our e2e tests
+  // declared in this scope so they can be accessed by all tests
+  // initialized in the `beforeAll` function
+  let utils: E2ETestUtils
+
   const filename = 'e2e_test_csv_dataset.csv'
   const datasetName = 'e2e_test_csv_datasetcsv'
 
@@ -36,11 +40,12 @@ describe('Qri End to End tests', function spec () {
       chromeDriverArgs: headless ? ['headless', 'no-sandbox', 'disable-dev-shm-usage'] : []
     })
     fakeDialog.apply(app)
+    utils = newE2ETestUtils(app)
     return app.start()
   })
 
   afterAll(async () => {
-    await delay(500)
+    await utils.delay(500)
     backend.close()
     if (app && app.isRunning()) {
       return app.stop()
@@ -56,6 +61,13 @@ describe('Qri End to End tests', function spec () {
   })
 
   it('accept terms of service by clicking accept and get taken to signup', async () => {
+    const {
+      atLocation,
+      waitForExist,
+      click,
+      exists
+    } = utils
+
     // Make sure the page is loaded
     await waitForExist('#welcome-page')
 
@@ -70,6 +82,14 @@ describe('Qri End to End tests', function spec () {
   })
 
   it('create a new account, taken to datasets page', async () => {
+    const {
+      atLocation,
+      waitForExist,
+      click,
+      setValue,
+      exists
+    } = utils
+
     // Make sure the page is loaded
     await waitForExist('#signup-page')
 
@@ -89,6 +109,14 @@ describe('Qri End to End tests', function spec () {
   })
 
   it('create new CSV dataset from a data source', async () => {
+    const {
+      atLocation,
+      click,
+      expectTextToBe,
+      onHistoryTab,
+      checkStatus
+    } = utils
+
     // make sure we are on the collection page
     await atLocation('#/collection')
 
@@ -124,83 +152,4 @@ describe('Qri End to End tests', function spec () {
     await checkStatus('body', 'added')
     await checkStatus('structure', 'added')
   })
-
-  // atLocation checks to see if the given hash location is where we expected it.
-  // Use this after a click that should have directed the user to a different
-  // route
-  const atLocation = async (expected: string) => {
-    const { client, browserWindow } = app
-    await client.waitUntil(async () => {
-      const currUrl: string = await browserWindow.getURL()
-      const location = new url.URL(currUrl).hash
-      return location === expected
-    }, 10000, `expected url to be '${expected}'`)
-  }
-
-  // waitForExist checks to see if an element exists on the page. If it does not
-  // exists after the timeout, it sends an error
-  const waitForExist = async (element: string) => {
-    const { client } = app
-    await client.waitUntil(async () => {
-      return client.element(element).isExisting()
-    }, 10000, `element '${element}' cannot be found`)
-  }
-
-  // click waits until the link is enabled and then clicks the element
-  const click = async (element: string) => {
-    await app.client.waitUntil(async () => {
-      const classes = await app.client.element(element).getAttribute('class')
-      return !(classes.includes('linkDisabled') || classes.includes('disabled'))
-    }, 10000)
-    await app.client.click(element)
-  }
-
-  // setValue sets the value of the element and waits for any debouncing
-  const setValue = async (element: string, value: any) => {
-    await app.client.element(element).setValue(value)
-  }
-
-  // exists iterates through the given selectors and checks to see that they
-  // exist on the pagge
-  const exists = async (elements: string[]) => {
-    elements.forEach(async (element: string) => {
-      expect(await app.client.element(element)).toBeDefined()
-    })
-  }
-
-  // doesNotExist checks that the element does not exist on the page
-  // specifically useful for when you want an element with an id to no longer
-  // have a particular class eg '#status-tab.disabled'
-  const doesNotExist = async (selector: string) => {
-    const err = await app.client.element(selector)
-    expect(err.type).toBe('NoSuchElement')
-  }
-
-  // expectTextToBe wraps expect().toBe()
-  const expectTextToBe = async (selector: string, text: string) => {
-    // await app.client.waitUntil(async () => {
-    //   return !!await app.client.element(selector)
-    // }, 10000, `element '${selector}' does not exist`)
-    expect(await app.client.element(selector).getText()).toBe(text)
-  }
-
-  // onHistoryTab uses onTab to check if the we are on the history tab
-  const onHistoryTab = async () => {
-    await onTab('history')
-  }
-
-  // onStatusTab uses onTab to check if the we are on the history tab
-  const onStatusTab = async () => {
-    await onTab('status')
-  }
-
-  // onTab checks to see if the element exists with the 'active' class
-  const onTab = async (tab: string) => {
-    expect(await app.client.element(`#${tab}-tab.active`)).toBeDefined()
-  }
-
-  // checkStatus ensures that component exists with the correct status dot class
-  const checkStatus = async (component: string, status: string) => {
-    expect(await app.client.element(`#${component}-status.status-dot-${status}`)).toBeDefined()
-  }
 })
