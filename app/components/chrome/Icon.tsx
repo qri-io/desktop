67fba53767226e1f67df9c7ba2e0780669f62227
ramfox <kasey@qri.io>
2020-01-08 13:35:58 -0500

    feat(icon): `Icon` component The `Icon` component should encompess all the necessary icons for the app. We are wrapping the FontAwesomeIcon component. However, some icons we need are not part of the free set of FontAwesomeIcons, so, for now, those icons contains the `faQuestionCircle` icon, and will be replaced with svgs asap. The icons that do not have equivalents, or otherwise need replacing, are: boolean, object, array, integer, number, string, null, commit

---
+++ app/components/chrome/Icon.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react'
+import classNames from 'classnames'
+
+import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
+import { 
+  faQuestionCircle, 
+  faFont, 
+  faHashtag, 
+  faCode, 
+  faGlasses, 
+  faLock, 
+  faTh, 
+  faSearch,
+  faQuestion,
+  faComment,
+  faCloudUploadAlt,
+  faFolderOpen,
+  faArchive,
+  faTags,
+  faPlus,
+  faDownload,
+  faFileAlt,
+  faCopy
+} from '@fortawesome/free-solid-svg-icons'
+
+interface IconProps {
+  // name of the icon
+  icon: string
+  // size sm: .875em
+  // md: 1.33em
+  // lg: 2em
+  size: 'sm' | 'md' | 'lg'
+  color: 'light' | 'medium' | 'dark'
+}
+
+const icons: {[key:  string]: any} = {
+  'search': faSearch,
+  'any': faQuestion,
+  'string': faFont,
+  'integer': faHashtag,
+  'number': faHashtag,
+  'boolean': faQuestionCircle,
+  'null': faQuestionCircle,
+  'object': faQuestionCircle,
+  'array': faQuestionCircle,
+  'chat': faComment,
+  'publish': faCloudUploadAlt,
+  'openInFinder': faFolderOpen,
+  'structure': faTh,
+  'unknown': faQuestionCircle,
+  'moreInfo': faQuestionCircle,
+  'body': faArchive,
+  'meta': faTags,
+  'create': faPlus,
+  'clone': faDownload,
+  'download': faDownload,
+  'dataset': faFileAlt,
+  'datasets': faCopy,
+  'readme': faGlasses,
+  'commit': faQuestionCircle,
+  'lock': faLock,
+  'transform': faCode,
+}
+
+export const iconsList = Object.keys(icons)
+
+const Icon: React.FunctionComponent<IconProps> = ({
+  icon = 'any',
+  size = 'md',
+  color = 'light'
+}) => {
+
+  const sizes: {[key: string]: FontAwesomeIconProps['size']} = {
+    'sm': 'sm',
+    'md': 'lg',
+    'lg': '2x'
+  }
+
+  return <FontAwesomeIcon size={sizes[size]} icon={icons[icon]} className={`icon-${color}`}/>
+}
+
+export default Icon
