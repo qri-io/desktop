d01a4591b8bf83c4efc54f11bf7e778c43f78d10
b5 <sparkle_pony_2000@qri.io>
2020-03-01 23:41:28 -0500

    feat(ErrorHandler): handle react errors, crash reports
    We've been getting complaints from users of a "blank white screen", which is caused by unhandled exceptions in react code.
    
    While we're learning from past lessons, we've continually gotten incredible ROI for time spent building tools that log and report errors. To that end, I've added a crash reporter to our error handler. Ideally this'll help us diagnose and address fixes faster without forcing users to send us data.

--- app/constants.js
+++ app/constants.js
@@ -1,8 +1,9 @@
 // these constants may be used by both the main process and frontend
 // so they need to be defined as CommonJS
+const BACKEND_URL = 'http://localhost:2503'
+const CRASH_REPORTER_URL = 'https://crashreports.qri.io/desktop'
 const DISCORD_URL = 'https://discordapp.com/invite/thkJHKj'
 const QRI_CLOUD_URL = 'https://qri.cloud'
-const BACKEND_URL = 'http://localhost:2503'
 const WEBSOCKETS_URL = 'ws://localhost:2506'
 const WEBSOCKETS_PROTOCOL = 'qri-websocket'
 // 3000ms is quick enough for the app to feel responsive
@@ -11,6 +12,7 @@ const DEFAULT_POLL_INTERVAL = 3000
 
 module.exports = {
   BACKEND_URL,
+  CRASH_REPORTER_URL,
   DEFAULT_POLL_INTERVAL,
   DISCORD_URL,
   QRI_CLOUD_URL,
