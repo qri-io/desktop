bcca2decc2d2c8b79b9fe55e86b4201f40eb3c7c
ramfox <kasey@qri.io>
2020-03-04 16:11:50 -0500

    style(TableDiff): fix scrolling and width

--- app/components/compare/TableDiff.tsx
+++ app/components/compare/TableDiff.tsx
@@ -21,7 +21,7 @@ const TableDiff: React.FunctionComponent<TableDiffProps> = ({ data }) => {
 
   let count = 0
   return (
-    <div className='table_diff'>
+    <div className='table-diff'>
       <div className='header'>
         {/* <b>{data.meta['---']}</b> */}
         <div style={{ 'float': 'right' }}>
@@ -29,81 +29,83 @@ const TableDiff: React.FunctionComponent<TableDiffProps> = ({ data }) => {
           <DiffStat data={data.stat} />
         </div>
       </div>
-      <table className='content'>
-        {data.schema && <TableDiffSchemaHead data={schema} />}
-        <tbody>
-          {data.diff.map((p, i) => {
-            if (Array.isArray(p[0])) {
-              count = 1
-              // leftCount = p[0][0][0]
-              // rightCount = p[0][1][0]
-              return (
-                <tr key={i}>
-                  <td colSpan={colCount} className='truncated'>
-                    <span>Additional Content Hidden</span>
-                  </td>
-                </tr>
-              )
-            }
-
-            let changeType
-            if (p[0] === '-') {
-              changeType = 'rem'
-              const open = (opened[`${i}`] || opened['all'])
-              if (open) {
+      <div id='table-diff-container'>
+        <table>
+          {data.schema && <TableDiffSchemaHead data={schema} />}
+          <tbody>
+            {data.diff.map((p, i) => {
+              if (Array.isArray(p[0])) {
+                count = 1
+                // leftCount = p[0][0][0]
+                // rightCount = p[0][1][0]
                 return (
-                  <tr key={i} className={'previous_row'} onClick={() => {
-                    const key = `${i}`
-                    const open = (opened[key])
-                    setOpened({ [key]: !open })
-                  }}>
-                    <td className="line_count rem">
-                      <div className="line_num">{count + 1}</div>
+                  <tr key={i}>
+                    <td colSpan={colCount} className='truncated'>
+                      <span>Additional Content Hidden</span>
                     </td>
-                    {p[2].map((el, i) => (<td key={i} className="rem">{el}</td>))}
                   </tr>
                 )
               }
 
-              return (
-                <tr key={i} className={'collapsed_row'} onClick={() => {
-                  const key = `${i}`
-                  const open = (opened[key])
-                  setOpened({ [key]: !open })
-                }}><td colSpan={colCount} className="rem"></td>
-                </tr>
-              )
-            } else if (p[0] === '+') {
-              count++
-              changeType = 'add'
-            } else {
-              count++
-              changeType = 'ctx'
-            }
+              let changeType
+              if (p[0] === '-') {
+                changeType = 'rem'
+                const open = (opened[`${i}`] || opened['all'])
+                if (open) {
+                  return (
+                    <tr key={i} className={'previous_row'} onClick={() => {
+                      const key = `${i}`
+                      const open = (opened[key])
+                      setOpened({ [key]: !open })
+                    }}>
+                      <td className="line_count rem">
+                        <div className="line_num">{count + 1}</div>
+                      </td>
+                      {p[2].map((el, i) => (<td key={i} className="rem">{el}</td>))}
+                    </tr>
+                  )
+                }
 
-            return (<tr key={i} className={changeType}>
-              <td className="line_count">
-                <div className="line_num">{count}</div>
-              </td>
-              {p.length === 3 && p[2].map((el, i) => {
-                return <td key={i}>{el}</td>
-              })}
-              {p.length === 4 && foldRowChanges(p[3]).map((change, i) => {
-                const row = count
-                const col = i
-                return <td key={i} className={change.type}>
-                  {change.prev && <div className={`${(opened[`${row}.${col}`] || opened['all']) ? 'previous_cell' : 'collapsed_cell'} rem`} onClick={() => {
-                    const key = `${row}.${col}`
+                return (
+                  <tr key={i} className={'collapsed_row'} onClick={() => {
+                    const key = `${i}`
                     const open = (opened[key])
                     setOpened({ [key]: !open })
-                  }}>{change.prev.value}</div>}
-                  {change.value}
+                  }}><td colSpan={colCount} className="rem"></td>
+                  </tr>
+                )
+              } else if (p[0] === '+') {
+                count++
+                changeType = 'add'
+              } else {
+                count++
+                changeType = 'ctx'
+              }
+
+              return (<tr key={i} className={changeType}>
+                <td className="line_count">
+                  <div className="line_num">{count}</div>
                 </td>
-              })}
-            </tr>)
-          })}
-        </tbody>
-      </table>
+                {p.length === 3 && p[2].map((el, i) => {
+                  return <td key={i}>{el}</td>
+                })}
+                {p.length === 4 && foldRowChanges(p[3]).map((change, i) => {
+                  const row = count
+                  const col = i
+                  return <td key={i} className={change.type}>
+                    {change.prev && <div className={`${(opened[`${row}.${col}`] || opened['all']) ? 'previous_cell' : 'collapsed_cell'} rem`} onClick={() => {
+                      const key = `${row}.${col}`
+                      const open = (opened[key])
+                      setOpened({ [key]: !open })
+                    }}>{change.prev.value}</div>}
+                    {change.value}
+                  </td>
+                })}
+              </tr>)
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
