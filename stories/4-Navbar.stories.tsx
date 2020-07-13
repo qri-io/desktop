a9e9ffd68f0ac5630837d8c2d7c79175fe59c5a0
Alicia <alicia.underhill@gmail.com>
2020-07-13 09:36:26 -0600

    fix(Navbar story): get Navbar story to run in Storybook

--- stories/4-Navbar.stories.tsx
+++ stories/4-Navbar.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import Navbar from '../app/components/nav/Navbar'
+import { NavbarComponent } from '../app/components/nav/Navbar'
 
 export default {
   title: 'Chrome | Navbar',
@@ -14,7 +14,9 @@ export const std = () => {
   return (
     <div style={{ width: 800, margin: '10px auto' }}>
       <Router>
-        <Navbar location='foo/bar' />
+        <Route render={(props) => {
+          return <NavbarComponent setModal={() => console.log("set modal!")} {...props} />
+        }}/>
       </Router>
     </div>
   )
