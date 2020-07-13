1963611fa816317ee3657c77c6791ac27290114b
Alicia <alicia.underhill@gmail.com>
2020-07-13 15:08:58 -0600

    fix(Network story): get Network story rendering

--- stories/Network.stories.tsx
+++ stories/Network.stories.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import feed from './data/splash'
+// import { Dataset as IDataset } from '../app/models/dataset'
+// import Navbar from '../app/components/nav/Navbar'
+import { NetworkHome } from '../app/components/network/NetworkHome'
+// import Dataset from '../app/components/dataset/Dataset'
+// import { ActionButtonProps } from '../app/components/chrome/ActionButton'
 
-import { Dataset as IDataset } from '../app/models/dataset'
-import { NetworkHomeData } from '../app/models/network'
-import Navbar from '../app/components/nav/Navbar'
-import NetworkHome from '../app/components/network/NetworkHome'
-import Dataset from '../app/components/dataset/Dataset'
-import { ActionButtonProps } from '../app/components/chrome/ActionButton'
-
-const cities = require('./data/cities.dataset.json')
+// const cities = require('./data/cities.dataset.json')
 
 export default {
   title: 'Network',
@@ -19,45 +16,12 @@ export default {
   }
 }
 
-interface FetchOptions {
-  method: string
-  headers: Record<string, string>
-  body?: string
-}
-
-async function homeFeed (): Promise<NetworkHomeData> {
-  const options: FetchOptions = {
-    method: 'GET'
-  }
-
-  const r = await fetch(`http://localhost:2503/feed/home`, options)
-  const res = await r.json()
-  if (res.meta.code !== 200) {
-    var err = { code: res.meta.code, message: res.meta.error }
-    throw err // eslint-disable-line
-  }
-
-  return res.data as NetworkHomeData
-}
-
 export const Home = () => {
-  const [loading, setLoading] = React.useState(true)
-  const [data, setData] = React.useState(null)
-  const [error, setError] = React.useState('')
-
-  React.useEffect(() => {
-    homeFeed()
-      .then(f => {
-        setData(f)
-        setLoading(false)
-      })
-      .catch(error => {
-        setLoading(false)
-        setError(error)
-      })
-  }, [false])
-
-  return <NetworkHome data={data} loading={loading} error={error} />
+  return (
+    <Router>
+      <Route render={(props) => <NetworkHome {...props} />} />
+    </Router>
+  )
 }
 
 Home.story = {
