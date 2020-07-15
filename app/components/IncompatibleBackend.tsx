101d50697b9ae80d57fc4f54bcc033f0a8fca715
Alicia <alicia.underhill@gmail.com>
2020-07-15 15:32:16 -0600

    refactor(IncompatibleBackend): update copy and add new line

--- app/components/IncompatibleBackend.tsx
+++ app/components/IncompatibleBackend.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import WelcomeTemplate from './onboard/WelcomeTemplate'
 import ExternalLink from './ExternalLink'
-import { backendVersion } from '../../version.js'
 
 interface IncompatibleBackendProps {
   incompatibleVersion: string
 }
 
+// TODO: this variable needs to be replaced when the
+// lowestCompatibleBackend variable exists in backend.js
+const lowestCompatibleBackend = '0.9.8'
+
 const IncompatibleBackend: React.FC<IncompatibleBackendProps> = ({ incompatibleVersion }) => (
   <WelcomeTemplate title='Incompatible Backend Version'>
-    <p>{`Incorrect backend version supplied. Expected qri v${backendVersion}, but have v${incompatibleVersion}. Please file an issue `}
-      <ExternalLink id="file-issue" href="https://github.com/qri-io/desktop/issues/new">here</ExternalLink>.
-    </p>
+    <p>{`The lowest supported backend version is v${lowestCompatibleBackend}, but you are running on v${incompatibleVersion}.`}</p>
+    <p>Please file an issue <ExternalLink id="file-issue" href="https://github.com/qri-io/desktop/issues/new">here</ExternalLink>.</p>
   </WelcomeTemplate>
 )
 
