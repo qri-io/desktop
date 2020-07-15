ed3124d14e5867197ed621e66314149e764d7ac6
Alicia <alicia.underhill@gmail.com>
2020-07-15 10:56:22 -0600

    feat(IncompatibleBackend): add IncompatibleBackend component

---
+++ app/components/IncompatibleBackend.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import WelcomeTemplate from './onboard/WelcomeTemplate'
+import ExternalLink from './ExternalLink'
+import { backendVersion } from '../../version.js'
+
+interface IncompatibleBackendProps {
+  incompatibleVersion: string
+}
+
+const IncompatibleBackend: React.FC<IncompatibleBackendProps> = ({ incompatibleVersion }) => (
+  <WelcomeTemplate title='Incompatible Backend Version'>
+    <p>{`Incorrect backend version supplied. Expected qri v${backendVersion}, but have ${incompatibleVersion}. Please file an issue `}
+      <ExternalLink id="file-issue" href="https://github.com/qri-io/desktop/issues/new">here</ExternalLink>.
+    </p>
+  </WelcomeTemplate>
+)
+
+export default IncompatibleBackend
