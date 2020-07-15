1c05571297c5d6e2a4c37b41bcb604e75d48a2f6
Alicia <alicia.underhill@gmail.com>
2020-07-15 14:11:15 -0600

    feat(MigrationFailed): create MigrationFailed component

---
+++ app/components/MigrationFailed.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import CrashReportSender from './CrashReportSender'
+
+const MigrationFailed: React.FC<{}> = () => <CrashReportSender title='The Qri backend migration failed!'/>
+
+export default MigrationFailed
