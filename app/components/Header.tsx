2c436ceb618d78e5e7f8331a1cd08307bf93752c
Alicia <alicia.underhill@gmail.com>
2020-07-22 09:19:49 -0600

    feat(Header): build new Header component with story

---
+++ app/components/Header.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { faPlus, faDownload } from '@fortawesome/free-solid-svg-icons'
+
+import { Modal, ModalType } from '../models/modals'
+
+import { connectComponentToProps } from '../utils/connectComponentToProps'
+
+import { setModal } from '../actions/ui'
+
+import HeaderColumnButton from './chrome/HeaderColumnButton'
+
+interface HeaderProps {
+  title: string
+
+  // setting action
+  setModal: (modal: Modal) => void
+}
+
+export const Header: React.FC<HeaderProps> = ({ title, setModal }) => (
+  <header className="header">
+    <h3 className="header-title">{title}</h3>
+    <div className="header-column">
+      <HeaderColumnButton
+        id="create-dataset"
+        icon={faPlus}
+        label="Create new Dataset"
+        onClick={() => setModal({ type: ModalType.CreateDataset })}
+      />
+      <HeaderColumnButton
+        id="add-dataset"
+        icon={faDownload}
+        label="Add existing Dataset"
+        onClick={() => setModal({ type: ModalType.AddDataset })}
+      />
+    </div>
+  </header>
+)
+
+export default connectComponentToProps(
+  Header,
+  {},
+  { setModal }
+)
