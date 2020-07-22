bf8722b7dcb38ae6c213e28506cc2967dc9da6ef
Alicia <alicia.underhill@gmail.com>
2020-07-22 11:09:23 -0600

    doc(*.web.tsx extensions): add comment to clarify why noop is needed for webpack build

--- app/components/platformSpecific/ExternalLink.web.tsx
+++ app/components/platformSpecific/ExternalLink.web.tsx
@@ -1,1 +1,4 @@
+// This file is needed for webpack to build ExternalLink for in-browser rendering without loading
+// the electron dependency. When ExternalLink is rendered on Web, this function is a noop
+// and the href is preferred.
 export const onClick = () => undefined
