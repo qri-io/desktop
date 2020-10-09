1d7ac10586356b05ad4ae6095a62db6e499bac16
b5 <sparkle_pony_2000@qri.io>
2020-10-09 15:39:29 -0400

    feat(CollectionHome): add "pull" to row hamburger menu
    * fix: remove "home" header from network home
    * fix: clicking an unpublished status icon in collection home no longer opens a link
    * fix: disable SQL / compare view in production AND dev environments

--- app/components/collection/collectionHome/StatusIcons.tsx
+++ app/components/collection/collectionHome/StatusIcons.tsx
@@ -16,9 +16,10 @@ const StatusIcons: React.FC<StatusIconsProps> = ({ data, onClickFolder }) => {
   return (
     <>
       <span className="dataset-link" data-tip={published ? 'published' : 'unpublished'}>
-        <ExternalLink href={`${QRI_CLOUD_URL}/${username}/${name}`}>
+        {published && <ExternalLink href={`${QRI_CLOUD_URL}/${username}/${name}`}>
           <Icon icon="publish" size="sm" color={published ? 'dark' : 'medium'}/>
-        </ExternalLink>
+        </ExternalLink>}
+        {!published && <Icon icon="publish" size="sm" color={published ? 'dark' : 'medium'}/>}
       </span>
       {onClickFolder && <a onClick={(e: React.MouseEvent) => onClickFolder(row, e)} className="dataset-link" data-tip={fsiPath ? 'working directory' : 'no working directory'}>
         <Icon icon="openInFinder" size="sm" color={fsiPath ? 'dark' : 'medium'}/>
