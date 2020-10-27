8275edebe2a1af0b942c5acef77cfb2304d9c6dc
ramfox <kasey@qri.io>
2020-10-27 17:26:12 -0400

    refactor(Collection): move dataset actions into `Dataset`
    We have some holdover from when Collection and Dataset were seperate views. Moved some listeners from Collection to Dataset, where it makes more sense for them to live.

--- app/components/collection/Dataset.tsx
+++ app/components/collection/Dataset.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
+import {
+  addRendererListener,
+  removeRendererListener,
+  openDirectory
+} from './platformSpecific/Dataset.TARGET_PLATFORM'
+
+import { Modal, ModalType } from '../../models/modals'
 import Store, { RouteProps } from '../../models/Store'
 import Dataset from '../../models/dataset'
 import { QriRef, qriRefFromRoute } from '../../models/qriRef'
 import { LaunchedFetchesAction } from '../../store/api'
 import { connectComponentToPropsWithRouter } from '../../utils/connectComponentToProps'
 import { fetchWorkbench } from '../../actions/workbench'
-import { selectDataset } from '../../selections'
+import { selectDataset, selectFsiPath, selectInNamespace, selectWorkingDatasetIsPublished } from '../../selections'
 import { setModal } from '../../actions/ui'
 
 import ComponentList from './ComponentList'
@@ -19,15 +26,54 @@ import LogList from './LogList'
 export interface DatasetProps extends RouteProps {
   qriRef: QriRef
   dataset: Dataset
+  isPublished: boolean
+  inNamespace: boolean
+  fsiPath: string
   fetchWorkbench: (qriRef: QriRef) => LaunchedFetchesAction
+  setModal: (modal: Modal) => void
 }
 
 export const DatasetComponent: React.FunctionComponent<DatasetProps> = (props) => {
   const {
     qriRef,
     dataset,
-    fetchWorkbench
+    isPublished,
+    fsiPath,
+    inNamespace,
+    fetchWorkbench,
+    setModal
   } = props
+
+  const publishUnpublishDataset = () => {
+    inNamespace && isPublished
+      ? setModal({
+        type: ModalType.UnpublishDataset,
+        username: qriRef.username,
+        name: qriRef.name
+      })
+      : setModal({
+        type: ModalType.PublishDataset,
+        username: qriRef.username,
+        name: qriRef.name
+      })
+  }
+
+  const openWorkingDirectory = () => {
+    openDirectory(fsiPath)
+  }
+
+  React.useEffect(() => {
+    if (addRendererListener) {
+      addRendererListener('open-working-directory', openWorkingDirectory)
+      addRendererListener('publish-unpublish-dataset', publishUnpublishDataset)
+    }
+    return () => {
+      if (removeRendererListener) {
+        removeRendererListener('open-working-directory', openWorkingDirectory)
+        removeRendererListener('publish-unpublish-dataset', publishUnpublishDataset)
+      }
+    }
+  }, [])
 
   React.useEffect(() => {
     fetchWorkbench(qriRef)
@@ -61,10 +107,14 @@ export const DatasetComponent: React.FunctionComponent<DatasetProps> = (props) =
 export default connectComponentToPropsWithRouter(
   DatasetComponent,
   (state: Store, ownProps: DatasetProps) => {
+    const qriRef = qriRefFromRoute(ownProps)
     return {
       ...ownProps,
-      qriRef: qriRefFromRoute(ownProps),
-      dataset: selectDataset(state)
+      qriRef,
+      dataset: selectDataset(state),
+      isPublished: selectWorkingDatasetIsPublished(state),
+      fsiPath: selectFsiPath(state),
+      inNamespace: selectInNamespace(state, qriRef)
     }
   },
   {
